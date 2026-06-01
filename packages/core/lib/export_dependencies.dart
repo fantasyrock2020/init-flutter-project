3d5cfe59789ac8bd4b6e504c8a471ca8a34116ea
Truong Tan Sang <fantasyrock2020@gmail.com>
2026-06-01 15:44:59 +0700

    feat: update pubspec

--- packages/core/lib/export_dependencies.dart
+++ packages/core/lib/export_dependencies.dart
@@ -3,5 +3,4 @@ export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:bloc/bloc.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
-export 'package:json_serializable/json_serializable.dart';
 export 'package:dio/dio.dart';
